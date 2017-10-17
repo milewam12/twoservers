395cf29b8ac8543a5b783a1ee0ac9b65a23f7980
Milena Garcés <milenagarces@Milenas-MacBook-Pro.local>
2017-10-17 19:54:12 -0400

    files2

---
+++ keys.js
@@ -0,0 +1,9 @@
+// console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: "VDvUJEOgucw5HD3GgqRyjSIRW",
+  consumer_secret: '0hMMyWjvmaPOlHvMBF2mmBmY62OTCwfeE2t3MYD3nm7A0CUY3a',
+  access_token_key: '879747751789113344-OhZPkOtRuNzN2B55K0FBaSJ1asZF1fX',
+  access_token_secret: 'tKYwus2XjRpnx2JYuG67tirAxoO8wGRRzcTLoWpL6yJ3T',
+}
+
--- twoServers.js
+++ twoServers.js
@@ -1,13 +1,51 @@
 var http = require ("http");
-var PORT = 7000;
+var twitter = require("twitter")
+var PORT1 = 7000;
 
-function handleRequest(request, response) {
+
+// TWITTER//
+var client = new Twitter(keys.twitterKeys);
+
+var params = { screen_name: 'AnaGarces' };
+client.get('statuses/user_timeline', params, function (error, tweets, response) {
     
-    response.end("Keep your face to the sunshine and you cannot see a shadow." + request.url);
+        if (error) {
+            return console.log("liri doesn't understand that commands");
+        }
+    
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                var date = tweets[i].created_at
+                var texts = tweets[i].text
+            }
+            console.log( date, texts)
+        }
+    
+    });
+
+
+function handleRequest1(request, response) {
+    
+    response.end( date, texts  
+        + request.url);
 }
 
-var server = http.createServer(handleRequest);
+var server1 = http.createServer(handleRequest1);
 
-server.listen(PORT, function () {
-    console.log("Server listening on: http://localhost:" + PORT);
+server1.listen(PORT1, function () {
+    console.log("Server listening on: http://localhost:" + PORT1);
+})
+
+
+var PORT2 = 7500;
+
+function handleRequest2(request, response) {
+    
+    response.end("When someone tells me 'no,' it doesn't mean I can't do it, it simply means I can't do it with them." + request.url);
+}
+
+var server2 = http.createServer(handleRequest2);
+
+server2.listen(PORT2, function () {
+    console.log("Server listening on: http://localhost:" + PORT2);
 })
